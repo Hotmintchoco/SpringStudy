312cf842412f08516df1b7d250b1709a2c79ee53
SpringStrar <yumkl123@gmail.com>
2022-06-15 21:08:42 +0900

    Spring 인프런 강의 1

---
+++ Day2022_0503/src/com/tv/TVCR.java
@@ -0,0 +1,22 @@
+package com.tv;
+
+public class TVCR extends Tv implements IVCR{
+	VCR vcr = new VCR();
+
+	@Override
+	public void play() { vcr.play(); }
+
+	@Override
+	public void stop() {vcr.stop();}
+
+	@Override
+	public void reset() {vcr.reset();}
+
+	@Override
+	public int getCounter() { return vcr.getCounter(); }
+
+	@Override
+	public void setCounter(int c) {
+		vcr.setCounter(c);
+	}
+}
