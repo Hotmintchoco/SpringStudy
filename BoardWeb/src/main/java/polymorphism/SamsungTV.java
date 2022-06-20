7d222fc51628dc06aed42a5f67e4109674d78314
SpringStrar <yumkl123@gmail.com>
2022-06-20 22:23:52 +0900

    스프링 생성자 생성

---
+++ BoardWeb/src/main/java/polymorphism/SamsungTV.java
@@ -0,0 +1,27 @@
+package polymorphism;
+
+public class SamsungTV implements TV{
+	private Speaker speaker;
+	
+	public SamsungTV() {
+		System.out.println("==> SamsungTV(1) 按眉 积己");
+	}
+	
+	public SamsungTV(Speaker speaker) {
+		System.out.println("==> SamsungTV(2) 按眉 积己");
+		this.speaker = speaker;
+	}
+
+	public void powerOn() {
+		System.out.println("SamsungTV ---- 傈盔 囊促.");
+	}
+	public void powerOff() {
+		System.out.println("SamsungTV ---- 傈盔 馋促.");
+	}
+	public void volumeUp() {
+		speaker.volumeUp();
+	}
+	public void volumeDown() {
+		speaker.volumeDown();
+	}
+}
