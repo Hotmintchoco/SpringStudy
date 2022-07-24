5a11a39e2fe67a58c92fa2b5f820b2d1d4f6177d
IceHotChoco <yumkl123@gmail.com>
2022-07-24 16:40:39 +0900

    api 크롤링 연습, jsoup 연습 및 비교 방법 확인

--- tripProject/src/main/java/com/trip/ex/feapi.java
+++ tripProject/src/main/java/com/trip/ex/feapi.java
@@ -1,79 +1,27 @@
 package com.trip.ex;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.net.URL;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.apache.jasper.tagplugins.jstl.core.If;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
-import com.trip.domain.FesDataDTO;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class feapi {
-	//url �����ؼ� xml ���� �������� parser�� ���ϴ� ���� �����ϴ� �۾�
 	
 	public static void main(String[] args){
-	DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-	try {
-		DocumentBuilder builder = factory.newDocumentBuilder();
-		// �������� �����ϴ� url ������ �����ؼ� ������ ��������
-		StringBuffer pharm_url = new StringBuffer();
-		pharm_url.append("http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchFestival");
-		pharm_url.append("?serviceKey=sWi23NfHCswD2JLkVzlAjWdx84T9hH%2B4%2BgYdeHg5rakMR397CZtjr1hoq8Mo56LMzSCjxrlMzLEkI0Bi%2FwFQ0Q%3D%3D&numOfRows=3&MobileOS=ETC&MobileApp=AppTest&arrange=A&listYN=Y&eventStartDate=20220701");
-
-		//�������� �������ϱ�
-		URL url = new URL(pharm_url.toString());
 		
-		
-		//�����ؼ� ������ �о����
-	BufferedInputStream xmldata = new BufferedInputStream(url.openStream());
-		Document document = builder.parse(xmldata);//inputstream ��ü�� ���·� �Ľ��� ������ �Ѱ��ش�
-		
-		Element root = document.getDocumentElement();
-		System.out.println(root.getTagName());
-		NodeList list = root.getElementsByTagName("item");
-		System.out.println(list.getLength());
-		
-		for(int i=0; i<list.getLength(); i++) {
-			Node node =list.item(i);
-			NodeList item_childlist = node.getChildNodes();
-			for(int j=0; j<item_childlist.getLength(); j++) {
-				Node item_child = item_childlist.item(j);
-//				System.out.print(item_child.getNodeName()+":"+item_child.getTextContent());
-//				System.out.print(",");
-			}
+		try {
+			for(int i=0; i<10; i++) {
+				Document doc = Jsoup.connect("http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchFestival"
+						+ "?serviceKey=sWi23NfHCswD2JLkVzlAjWdx84T9hH%2B4%2BgYdeHg5rakMR397CZtjr1hoq8Mo56LMzSCjxrlMzLEkI0Bi%2FwFQ0Q%3D%3D&MobileOS=ETC&MobileApp=AppTest&arrange=A&listYN=Y&eventStartDate=20220701").get();
+				Elements title = doc.select("item title");
+				
+				System.out.println("번호 : " + (i+1));
+				System.out.println(title.get(i).text());
+			}		
+					
 			
-		System.out.println();
-			for(int j=0; j<item_childlist.getLength(); j++) {
-				String title = "";
-				Node item_child = item_childlist.item(j);
-				
-				FesDataDTO dto = new FesDataDTO();
-				
-				if(item_child.getNodeName().equals("title")) {
-					if(item_child.getTextContent() != null) {
-						title = item_child.getTextContent();
-						dto.setTitle(title);
-					}
-				}
-				System.out.println(dto.getTitle());
-			}
+		}catch (Exception e) {
+			e.printStackTrace();
 		}
-		
-	}catch (ParserConfigurationException e) {
-		e.printStackTrace();
-	}catch (SAXException e) {
-		e.printStackTrace();
-	}catch(IOException e) {
-		e.printStackTrace();
-	}
 	}
 }
--- tripProject/src/test/java/com/trip/mapper/FesDataMapperTests.java
+++ tripProject/src/test/java/com/trip/mapper/FesDataMapperTests.java
@@ -1,24 +1,17 @@
 package com.trip.mapper;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.net.URL;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import java.util.Iterator;
 
 import org.apache.jasper.tagplugins.jstl.core.If;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 import com.trip.domain.FesDataDTO;
 
@@ -33,92 +26,66 @@ public class FesDataMapperTests {
 	
 	@Test
 	public void testInsert() {
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		try {
-			DocumentBuilder builder = factory.newDocumentBuilder();
-			// �������� �����ϴ� url ������ �����ؼ� ������ ��������
-			StringBuffer pharm_url = new StringBuffer();
-			pharm_url.append("http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchFestival");
-			pharm_url.append("?serviceKey=sWi23NfHCswD2JLkVzlAjWdx84T9hH%2B4%2BgYdeHg5rakMR397CZtjr1hoq8Mo56LMzSCjxrlMzLEkI0Bi%2FwFQ0Q%3D%3D&MobileOS=ETC&MobileApp=AppTest&arrange=A&listYN=Y&eventStartDate=20220701");
-	
-			//�������� �������ϱ�
-			URL url = new URL(pharm_url.toString());
+			int k=1;
+			Document doc = Jsoup.connect("http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchFestival"
+					+ "?serviceKey=sWi23NfHCswD2JLkVzlAjWdx84T9hH%2B4%2BgYdeHg5rakMR397CZtjr1hoq8Mo56LMzSCjxrlMzLEkI0Bi%2FwFQ0Q%3D%3D&MobileOS=ETC&MobileApp=AppTest&arrange=A&listYN=Y&eventStartDate=20220701").get();
+			Elements all = doc.select("item");
+			Elements el = doc.getElementsByAttribute("firstimage");
+			int i = 3;
 			
-			
-			//�����ؼ� ������ �о����
-		BufferedInputStream xmldata = new BufferedInputStream(url.openStream());
-			Document document = builder.parse(xmldata);//inputstream ��ü�� ���·� �Ľ��� ������ �Ѱ��ش�
-			
-			Element root = document.getDocumentElement();
-			System.out.println(root.getTagName());
-			NodeList list = root.getElementsByTagName("item");
-			System.out.println(list.getLength());
-			
-			for(int i=0; i<list.getLength(); i++) {
-				Node node =list.item(i);
-				NodeList item_childlist = node.getChildNodes();
+//			Elements row = doc.select("item");
+//			for(Element row2 : row){
+//				Iterator<Element> it = row2.getElementsByTag("title").iterator();
+//				while(it.hasNext()) {
+//					String title = it.next().text();
+//					System.out.println(title);
+//					}
+//				} 
+			if(i == 3) {
 				
-				for(int j=0; j<item_childlist.getLength(); j++) {
-					String title="", address="", endDate="", startDate="",
-							firstImage="", mapX="", mapY="";
-					int mLevel= 0;
-					Node item_child = item_childlist.item(j);
-//					System.out.print(item_child.getNodeName()+":"+item_child.getTextContent());
-//					System.out.print(",");
-					
-					FesDataDTO dto = new FesDataDTO();
-					
-					if(item_child.getNodeName().equals("title")) {
-						title = item_child.getTextContent();
-					}
-					if(title.length()>=5)
-						dto.setTitle(title);
-					if(item_child.getNodeName().equals("addr1")) {
-						address = item_child.getTextContent();
-						if(address.length()>=1)
-						dto.setAddress(address);
-					}
-					if(item_child.getNodeName().equals("eventenddate")) {
-						endDate = item_child.getTextContent();
-						if(endDate.length()>=1)
-						dto.setEndDate(endDate);
-					}
-					if(item_child.getNodeName().equals("eventstartdate")) {
-						startDate = item_child.getTextContent();
-						if(startDate.length()>=1)
-						dto.setStartDate(startDate);
-					}
-					if(item_child.getNodeName().equals("firstimage")) {
-						firstImage = item_child.getTextContent();
-						if(firstImage.length()>=1)
-						dto.setFirstImg(firstImage);
-					}
-					if(item_child.getNodeName().equals("mapx")) {
-						mapX  = item_child.getTextContent();
-						if(mapX.length()>=1)
-						dto.setMapX(mapX);
-					}
-					if(item_child.getNodeName().equals("mapy")) {
-						mapY = item_child.getTextContent();
-						if(mapY.length()>=1)
-						dto.setMapY(mapY);
-					}
-					if(item_child.getNodeName().equals("mlevel")) {
-						mLevel = Integer.parseInt(item_child.getTextContent());
-						if(mLevel>=0)
-						dto.setMLevel(mLevel);
-					}
-					System.out.println(dto.getTitle());
-//					mapper.insert(dto);
+				
+				Elements title = all.select("title");
+				Elements address = all.select("addr1");
+				Elements startDate = all.select("eventstartdate");
+				Elements endDate = all.select("eventenddate");
+				Elements firstImg = all.select("firstimage");
+				Elements mapX = all.select("mapx");
+				Elements mapY = all.select("mapy");
+				Elements mLevel = all.select("mlevel");
+				
+				System.out.println("번호 : " + (i+1));
+				FesDataDTO dto = new FesDataDTO();
+				dto.setTitle(title.get(i).text());
+				dto.setAddress(address.get(i).text());
+				dto.setStartDate(startDate.get(i).text());
+				dto.setEndDate(endDate.get(i).text());
+//				if(firstImg.get(i).text() == null) {
+//					System.out.println("null값 발생 : " + k++);
+//				}
+//				dto.setFirstImg(firstImg.get(i).text());
+				dto.setMapX(mapX.get(i).text());
+				dto.setMapY(mapY.get(i).text());
+//				dto.setMLevel(Integer.parseInt(mLevel.get(i).text()));
+				
+				
+//				System.out.println(dto.getTitle());
+//				System.out.println(dto.getAddress());
+//				System.out.println(dto.getStartDate());
+//				System.out.println(dto.getEndDate());
+//				System.out.println(dto.getFirstImg());
+//				System.out.println(dto.getMapX());
+//				System.out.println(dto.getMapY());
+//				System.out.println(dto.getMLevel());
+				String imgTmp = all.get(i).html();
+				System.out.println(imgTmp);
+				if (imgTmp.contains("firstimage")) {
+					System.out.println("이미지 존재");
 				}
-//			System.out.println();
-			}
+			}		
+					
 			
-		}catch (ParserConfigurationException e) {
-			e.printStackTrace();
-		}catch (SAXException e) {
-			e.printStackTrace();
-		}catch(IOException e) {
+		}catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
