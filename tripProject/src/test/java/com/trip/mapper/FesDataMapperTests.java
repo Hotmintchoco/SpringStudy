b4b63c1847eee7d2931141391aa39a1624c87760
IceHotChoco <yumkl123@gmail.com>
2022-07-24 17:00:47 +0900

    api 크롤링 설정 완료

--- tripProject/src/test/java/com/trip/mapper/FesDataMapperTests.java
+++ tripProject/src/test/java/com/trip/mapper/FesDataMapperTests.java
@@ -29,10 +29,9 @@ public class FesDataMapperTests {
 		try {
 			int k=1;
 			Document doc = Jsoup.connect("http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchFestival"
-					+ "?serviceKey=sWi23NfHCswD2JLkVzlAjWdx84T9hH%2B4%2BgYdeHg5rakMR397CZtjr1hoq8Mo56LMzSCjxrlMzLEkI0Bi%2FwFQ0Q%3D%3D&MobileOS=ETC&MobileApp=AppTest&arrange=A&listYN=Y&eventStartDate=20220701").get();
+					+ "?serviceKey=sWi23NfHCswD2JLkVzlAjWdx84T9hH%2B4%2BgYdeHg5rakMR397CZtjr1hoq8Mo56LMzSCjxrlMzLEkI0Bi%2FwFQ0Q%3D%3D&MobileOS=ETC&numOfRows=100&MobileApp=AppTest&arrange=A&listYN=Y&eventStartDate=20220701").get();
 			Elements all = doc.select("item");
 			Elements el = doc.getElementsByAttribute("firstimage");
-			int i = 3;
 			
 //			Elements row = doc.select("item");
 //			for(Element row2 : row){
@@ -42,17 +41,24 @@ public class FesDataMapperTests {
 //					System.out.println(title);
 //					}
 //				} 
-			if(i == 3) {
+			Elements title = all.select("title");
+			Elements address = all.select("addr1");
+			Elements startDate = all.select("eventstartdate");
+			Elements endDate = all.select("eventenddate");
+			Elements firstImg = all.select("firstimage");
+			Elements mapX = all.select("mapx");
+			Elements mapY = all.select("mapy");
+			Elements mLevel = all.select("mlevel");
+			
+			for(int i=0; i<10; i++) {
 				
+				String imgTmp = all.get(i).html();
+				if (!(imgTmp.contains("firstimage"))) {
+					Element elImg = new Element("firstimage");
+					firstImg.add(i, elImg);
+					System.out.println("noimage");
+				}
 				
-				Elements title = all.select("title");
-				Elements address = all.select("addr1");
-				Elements startDate = all.select("eventstartdate");
-				Elements endDate = all.select("eventenddate");
-				Elements firstImg = all.select("firstimage");
-				Elements mapX = all.select("mapx");
-				Elements mapY = all.select("mapy");
-				Elements mLevel = all.select("mlevel");
 				
 				System.out.println("번호 : " + (i+1));
 				FesDataDTO dto = new FesDataDTO();
@@ -60,28 +66,20 @@ public class FesDataMapperTests {
 				dto.setAddress(address.get(i).text());
 				dto.setStartDate(startDate.get(i).text());
 				dto.setEndDate(endDate.get(i).text());
-//				if(firstImg.get(i).text() == null) {
-//					System.out.println("null값 발생 : " + k++);
-//				}
-//				dto.setFirstImg(firstImg.get(i).text());
+				dto.setFirstImg(firstImg.get(i).text());
 				dto.setMapX(mapX.get(i).text());
 				dto.setMapY(mapY.get(i).text());
 //				dto.setMLevel(Integer.parseInt(mLevel.get(i).text()));
 				
 				
-//				System.out.println(dto.getTitle());
-//				System.out.println(dto.getAddress());
-//				System.out.println(dto.getStartDate());
-//				System.out.println(dto.getEndDate());
-//				System.out.println(dto.getFirstImg());
-//				System.out.println(dto.getMapX());
-//				System.out.println(dto.getMapY());
+				System.out.println(dto.getTitle());
+				System.out.println(dto.getAddress());
+				System.out.println(dto.getStartDate());
+				System.out.println(dto.getEndDate());
+				System.out.println(dto.getFirstImg());
+				System.out.println(dto.getMapX());
+				System.out.println(dto.getMapY());
 //				System.out.println(dto.getMLevel());
-				String imgTmp = all.get(i).html();
-				System.out.println(imgTmp);
-				if (imgTmp.contains("firstimage")) {
-					System.out.println("이미지 존재");
-				}
 			}		
 					
 			
